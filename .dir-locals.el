ceb8dff38447d981bd9294530e4e4bcb1e987e85
Jean Lopes <11708686+jean-lopes@users.noreply.github.com>
2025-12-13 20:25:11 -0300

    add: rate-limiting post

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+((nil . ((org-publish-project-alist . (("blog"
+                                        :base-directory "org/"
+                                        :publishing-directory "docs/"
+                                        :publishing-function org-html-publish-to-html
+                                        :recursive t
+                                        :with-toc t
+                                        :section-numbers nil
+                                        :with-author nil
+                                        :with-date t
+                                        :with-creator nil
+                                        :time-stamp-file nil))))))
